4d1b41a5589aa7cba76f4498ac769c8e9a1182b3
Lorraine Ebanks <130781140+lorr010@users.noreply.github.com>
2026-02-04 10:14:34 -0500

    oh no
    oh no

--- src/test/java/org/example/Wrong.java
+++ src/test/java/org/example/Wrong.java
@@ -1,5 +1,7 @@
 public class Wrong {
     public static void main(String[] args) {
         System.out.println("This is the Wrong class.");
+        System.out.println("This is the Wrong class.");
     }
 }
+
